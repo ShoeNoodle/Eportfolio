0f441b4d62caf575b091087f36fe6d8bc62936d3
ShoeNoods <50181800+ShoeNoods@users.noreply.github.com>
2020-12-20 21:14:04 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 1000);
+    var intervalID = window.setInterval(5000);
 
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
