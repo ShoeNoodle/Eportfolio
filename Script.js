4d46beb5e2b8af4eb829f3837eaea9ea1714d9d4
ShoeNoods <50181800+ShoeNoods@users.noreply.github.com>
2022-04-06 16:47:00 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,9 +5,9 @@ window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 function showDiv() {
-   document.getElementById('Enigmadiv').style.display = "block";
-        dismissible: true, 
-        opacity: 0.5, 
-        in_duration: 300, 
-        out_duration: 200, 
-});
+        document.getElementById('Enigmadiv').style.display = "block"
+        dismissible: true,
+        opacity= 3, 
+        in_duration= 300, 
+        out_duration= 200;
+          }
