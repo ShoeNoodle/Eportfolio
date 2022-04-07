42b76ba4e386b885b85bc099e963e8c897df6ecd
ShoeNoods <50181800+ShoeNoods@users.noreply.github.com>
2022-04-07 19:01:42 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,9 +4,12 @@ setTimeout(function onReady() {
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
+  document.body.classList.add("stop-scrolling");
 }
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+  document.body.classList.remove("stop-scrolling");
 }
