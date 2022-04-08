d0c5defbf45f6d9491cd3ce9c95e6fd9375bec34
ShoeNoods <50181800+ShoeNoods@users.noreply.github.com>
2022-04-08 13:29:25 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -53,3 +53,17 @@ function closeForm6() {
   document.getElementById("myForm6").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
