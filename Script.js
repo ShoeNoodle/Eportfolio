d6cb254732cbdfbd0227a110fa087a157a0fef28
ShoeNoods <50181800+ShoeNoods@users.noreply.github.com>
2022-04-09 15:53:42 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,84 +10,84 @@ function openForm() {
   document.body.classList.add("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(4px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(4px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(4px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(4px)";
 }
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
   document.body.classList.remove("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(0px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(0px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(0px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(0px)";
 }
 function openForm2() {
   document.getElementById("myForm2").style.display = "block";
   document.body.classList.add("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(4px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(4px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(4px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(4px)";
 }
 function closeForm2() {
   document.getElementById("myForm2").style.display = "none";
   document.body.classList.remove("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(0px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(0px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(0px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(0px)";
 }
 function openForm3() {
   document.getElementById("myForm3").style.display = "block";
   document.body.classList.add("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(4px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(4px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(4px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(4px)";
 }
 function closeForm3() {
   document.getElementById("myForm3").style.display = "none";
   document.body.classList.remove("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(0px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(0px)"; 
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(0px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(0px)";
 }
 function openForm4() {
   document.getElementById("myForm4").style.display = "block";
   document.body.classList.add("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(4px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(4px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(4px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(4px)";
 }
 function closeForm4() {
   document.getElementById("myForm4").style.display = "none";
   document.body.classList.remove("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(0px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(0px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(0px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(0px)";
 }
 function openForm5() {
   document.getElementById("myForm5").style.display = "block";
   document.body.classList.add("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(4px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(4px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(4px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(4px)";
 }
 function closeForm5() {
   document.getElementById("myForm5").style.display = "none";
   document.body.classList.remove("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(0px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(0px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(0px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(0px)";
 }
 function openForm6() {
   document.getElementById("myForm6").style.display = "block";
   document.body.classList.add("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(4px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(4px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(4px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(4px)";
 }
 function closeForm6() {
   document.getElementById("myForm6").style.display = "none";
   document.body.classList.remove("stop-scrolling");
   document.getElementById("Enigma").style.webkitFilter = "blur(0px)";
   document.getElementById("Project_heading").style.webkitFilter = "blur(0px)";
-  document.getElementByClassName("navbar").style.webkitFilter = "blur(0px)";
+  document.getElementByClassName("navigationbar").style.webkitFilter = "blur(0px)";
 }
 let slideIndex = [1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
