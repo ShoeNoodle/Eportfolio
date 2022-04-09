23f83a10ff3453d74c51a22c5be42bd7ee276ac2
ShoeNoods <50181800+ShoeNoods@users.noreply.github.com>
2022-04-09 15:04:33 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -6,50 +6,62 @@ window.onbeforeunload = function () {
 }
 
 function openForm() {
+          document.getElementById("Project_overlay").style.display = "block";
   document.getElementById("myForm").style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 function closeForm() {
+          document.getElementById("Project_overlay").style.display = "none"; 
   document.getElementById("myForm").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
 function openForm2() {
+          document.getElementById("Project_overlay").style.display = "block";
   document.getElementById("myForm2").style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 function closeForm2() {
+          document.getElementById("Project_overlay").style.display = "none"; 
   document.getElementById("myForm2").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
 function openForm3() {
+          document.getElementById("Project_overlay").style.display = "block";
   document.getElementById("myForm3").style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 function closeForm3() {
+          document.getElementById("Project_overlay").style.display = "none"; 
   document.getElementById("myForm3").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
 function openForm4() {
+          document.getElementById("Project_overlay").style.display = "block";
   document.getElementById("myForm4").style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 function closeForm4() {
+          document.getElementById("Project_overlay").style.display = "none"; 
   document.getElementById("myForm4").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
 function openForm5() {
+          document.getElementById("Project_overlay").style.display = "block";
   document.getElementById("myForm5").style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 function closeForm5() {
+          document.getElementById("Project_overlay").style.display = "none"; 
   document.getElementById("myForm5").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
 function openForm6() {
+  document.getElementById("Project_overlay").style.display = "block";
   document.getElementById("myForm6").style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 function closeForm6() {
+          document.getElementById("Project_overlay").style.display = "none"; 
   document.getElementById("myForm6").style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
